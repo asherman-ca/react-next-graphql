f4fe97a5ca174fadb3d72fbff156166a02671913
Alex Sherman <asherman.ca@gmail.com>
2020-04-19 16:34:16 -0700

    fullstack coms

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,37 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  // this is for server side rendering
+  // crawls the entire page and does any fetching required
+  // not necessary for client rendered app
+  // getinitialprops is a nextjs lifecycle method
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-
-
-export default MyApp;
+// makes apollo client accessible via props
+export default withData(MyApp);

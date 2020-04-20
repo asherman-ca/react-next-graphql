79fee568168d0f3b02749ecccab48c01d8e9f2b8
Alex Sherman <asherman.ca@gmail.com>
2020-04-20 15:57:23 -0700

    item delete wired

--- components/Item.js
+++ components/Item.js
@@ -5,6 +5,7 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteButton from './DeleteItem';
 
 
 export default class Item extends Component {
@@ -39,7 +40,7 @@ export default class Item extends Component {
           <a>Edit ✏️</a>
         </Link>
         <button>Add to cart</button>
-        <button>Delete</button>
+        <DeleteButton id={item.id}>Delete</DeleteButton>
       </div>
     </ItemStyles>
   }
--- components/UpdateItem.js
+++ components/UpdateItem.js
@@ -115,7 +115,7 @@ export default class UpdateItem extends Component {
                       onChange={this.handleChange}
                       />
                   </label>
-                  <button type="submit">Save Changes</button>
+                  <button type="submit">Sav{loading ? 'ing' : 'e'} Changes</button>
                 </fieldset>
               </Form>
             )}

8349a2886ad7cf592d3babc8416d94e32beb74cc
Alex Sherman <asherman.ca@gmail.com>
2020-04-27 08:55:13 -0700

    wip

---
+++ components/Signup.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+
+const SIGNUP_MUTATION =  gql`
+  mutation SIGNUP_MUTATION(
+    $name: String!
+    $email: String!
+    $password: String!
+  ) {
+    signup(
+      name: $name
+      email: $email
+      password: $password
+    ) {
+      id
+    }
+  }
+`
+
+export default class Signup extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  saveToState = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <Form>
+        <fieldset>
+          <h2>Sign up for an account</h2>
+            <label htmlFor="email">
+              Email
+              <input 
+                type="email"
+                name="email"
+                placeholder="email"
+                value={this.state.email}
+                onChange={this.saveToState}
+              />
+            </label>  
+            <label htmlFor="name">
+              Name
+              <input 
+                type="text"
+                name="name"
+                placeholder="name"
+                value={this.state.name}
+                onChange={this.saveToState}
+              />
+            </label>
+            <label htmlFor="password">
+              Password
+              <input 
+                type="password"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.saveToState}
+              />
+            </label>
+
+            <button type="submut">Sign Up</button>
+        </fieldset>
+      </Form>
+    )
+  }
+}

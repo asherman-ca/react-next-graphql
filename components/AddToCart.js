635803c79b1b6764ad631bba8fae62145aaeb016
Alex Sherman <asherman.ca@gmail.com>
2020-04-30 11:19:15 -0700

    cart item component

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -16,8 +17,19 @@ export default class AddToCart extends Component {
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{id}}>
-        {addToCart => <button onClick={addToCart}>Add To Cart 🛒</button>}
+      <Mutation 
+        mutation={ADD_TO_CART_MUTATION} 
+        variables={{id}}
+        // the fetch updates the user, which updates the cart because cart is connected to the user query
+        refetchQueries={[
+          { query: CURRENT_USER_QUERY }
+        ]}
+      >
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart 🛒
+          </button>
+        )}
       </Mutation>
     )
   }
--- components/User.js
+++ components/User.js
@@ -11,6 +11,7 @@ const CURRENT_USER_QUERY = gql`
       permissions
       cart {
         id
+        quantity
         item {
           id
           price

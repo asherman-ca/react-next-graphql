ee7818fc86e4df60e69da9a995ebfbc9a06e5648
Alex Sherman <asherman.ca@gmail.com>
2020-05-04 15:10:25 -0700

    animation update

--- components/CartCount.js
+++ components/CartCount.js
@@ -8,23 +8,30 @@ const AnimationStyles = styled.span`
   .count {
     display: block;
     position: relative;
-    transition: all 0.4s;
+    /* transition: all 0.4s; */
     backface-visibility: hidden;
+    box-shadow: ${props => props.theme.bs};
   }
-  /* Initial State of the entered Dot */
+  /* Initial State of the entered Dot, starts turned away from us */
   .count-enter {
-    transform: scale(4) rotateX(0.5turn);
+    opacity: .5;
+    transform: rotateX(-0.5turn);
   }
+  /* rotates to facing us */
   .count-enter-active {
+    opacity: 1;
     transform: rotateX(0);
+    transition: all 1000ms;
   }
   .count-exit {
     top: 0;
     position: absolute;
-    transform: rotateX(0);
+    opacity: 1;
   }
   .count-exit-active {
-    transform: scale(4) rotateX(0.5turn);
+    opacity: .5;
+    transform: rotateX(0.5turn);
+    transition: all 300ms;
   }
 `;
 
@@ -37,12 +44,14 @@ const Dot = styled.div`
   min-width: 3rem;
   margin-left: 1rem;
   font-weight: 100;
+  /* this maintains a constant div width for all numbers as some are naturally wider than others */
   font-feature-settings: 'tnum';
   font-variant-numeric: tabular-nums;
 `;
 
 const CartCount = ({ count }) => (
   <AnimationStyles>
+    {/* transition group applies our animation at the time of rerender, when a new count prop is passed in */}
     <TransitionGroup>
       <CSSTransition
         unmountOnExit
